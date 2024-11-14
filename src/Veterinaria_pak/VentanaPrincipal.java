e990c7b16b2affa4734ccede6d73423a64d24d93
Matias Velozo <matias.velozo.4c@gmail.com>
2024-11-13 23:44:01 -0300

    agregando cosas

--- src/Veterinaria_pak/VentanaPrincipal.java
+++ src/Veterinaria_pak/VentanaPrincipal.java
@@ -1,6 +1,7 @@
 package Veterinaria_pak;
 
 import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.GradientPaint;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -13,7 +14,6 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.border.EmptyBorder;
-import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 
 public class VentanaPrincipal extends JFrame {
@@ -42,24 +42,37 @@ public class VentanaPrincipal extends JFrame {
         JButton btnModificar = new JButton("Modificar");
         JButton btnEliminar = new JButton("Eliminar");
         JButton btnDetalles = new JButton("Detalles");
+
         btnAgregar.setBounds(100, 500, 90, 40);
+        btnAgregar.setCursor(new Cursor(HAND_CURSOR));
         btnAgregar.setFocusable(false);
+
         btnModificar.setBounds(210, 500, 90, 40);
+        btnModificar.setCursor(new Cursor(HAND_CURSOR));
         btnModificar.setFocusable(false);
+
         btnEliminar.setBounds(320, 500, 90, 40);
+        btnEliminar.setCursor(new Cursor(HAND_CURSOR));
         btnEliminar.setFocusable(false);
+
         btnDetalles.setBounds(430, 500, 90, 40);
+        btnDetalles.setCursor(new Cursor(HAND_CURSOR));
         btnDetalles.setFocusable(false);
+
         panel1.add(btnAgregar);
         panel1.add(btnModificar);
         panel1.add(btnEliminar);
         panel1.add(btnDetalles);
+
         btnAgregar.setBackground( new Color(88, 219, 164));
         btnAgregar.setForeground(new Color(17, 25, 27));
+
         btnEliminar.setBackground(new Color(196, 81, 131));
         btnEliminar.setForeground(new Color(17, 25, 27));
+
         btnDetalles.setBackground(new Color(255, 111, 71));
         btnDetalles.setForeground(new Color(17, 25, 27));
+
         btnModificar.setBackground(new Color(255, 199, 69));
         btnModificar.setForeground(new Color(17, 25, 27));
 
@@ -93,20 +106,18 @@ public class VentanaPrincipal extends JFrame {
             }
         };
 
-        table = new JTable(dtm);
-        table.setOpaque(false);
-        table.setBackground(new Color(0, 0, 0, 0));
-        //DefaultTableColumnModel)
         
-        ((DefaultTableCellRenderer) table.getDefaultRenderer(Object.class)).setOpaque(false);
+        table = new JTable(dtm);
+        table.setBackground(new Color(0, 0, 0, 0));
+        table.setOpaque(false);
         table.setForeground(new Color(220, 220, 220));
         table.setSelectionForeground(new Color(255, 199, 69));
-
-        scroll = new JScrollPane(table);
         table.setModel(dtm);
-        scroll.setBounds(40, 40, 500, 400);
-        scroll.setOpaque(false); 
+
         
+        scroll = new JScrollPane(table);
+        scroll.setBounds(40, 40, 500, 400);
+        scroll.setOpaque(false);         
         scroll.setBorder(new EmptyBorder(0,0,0,0));
         scroll.getViewport().setOpaque(false);
 
@@ -122,7 +133,6 @@ public class VentanaPrincipal extends JFrame {
                 Graphics2D g2d = (Graphics2D) g;
                 int width = getWidth();
                 int height = getHeight();
-                // Crear el degradado desde el color inicial hasta el color final
                 GradientPaint gradient = new GradientPaint(0, 0, new Color(93, 131, 136), 0, height,
                         new Color(17, 25, 27));
                 g2d.setPaint(gradient);
